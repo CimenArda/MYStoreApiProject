3c37dd0de4ae4eb7179ec41d101cb302045ee074
ArdaCimen <cimenarda.yaz@gmail.com>
2024-12-15 13:17:20 +0300

    Ana Tema Contact ve About Consume İşlemleri

--- StoreProject.WebUI/Controllers/AboutController.cs
+++ StoreProject.WebUI/Controllers/AboutController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DtoLayer.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace StoreProject.WebUI.Controllers
 {
     public class AboutController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AboutController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7110/api/Abouts");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+
+
+                return View(value);
+
+            }
             return View();
+
         }
     }
 }
---
+++ StoreProject.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,42 @@
+﻿using DtoLayer.CategoryDtos;
+using DtoLayer.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace StoreProject.WebUI.Controllers
+{
+	public class ContactController : Controller
+	{
+
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public ContactController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+
+		[HttpPost]
+		public async Task<IActionResult> Index(CreateContactDto createContactDto)
+		{
+
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(createContactDto);
+			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PostAsync("https://localhost:7110/api/Contacts", stringContent);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index", "HomePage");
+			}
+			return View();
+		}
+	}
+}
