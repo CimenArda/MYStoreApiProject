f20733c9897de28edcc6f9df45ec81f75b2e3ddf
ArdaCimen <cimenarda.yaz@gmail.com>
2024-12-15 12:36:47 +0300

    Main Layout İşlemleri Admin Tarafında Sidebar Düzenlemeleri

---
+++ StoreProject.WebUI/ViewComponents/MainLayoutViewComponents/_MainLayoutFooterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoreProject.WebUI.ViewComponents.MainLayoutViewComponents
+{
+	public class _MainLayoutFooterComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
